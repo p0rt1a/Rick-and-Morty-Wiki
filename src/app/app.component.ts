d11f279a9fd6ee4f58306f8ace524c2b91436129
Alperen Polat <m_alperenPolat@hotmail.com>
2022-10-10 18:03:55 +0300

    previous - next page buttons created and responsive tested

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,14 @@ import { PostsService } from './services/posts.service';
 export class AppComponent implements OnInit {
   private url: string = 'https://rickandmortyapi.com/api/character';
   public data: any;
+  private pageCount: number = 0;
 
   constructor(private postsService: PostsService) {}
 
   ngOnInit(): void {
     this.postsService.getItems(this.url).subscribe((response) => {
       this.data = response;
+      console.log(this.data);
     });
   }
 
@@ -22,5 +24,27 @@ export class AppComponent implements OnInit {
     this.postsService.getItems(this.url, name).subscribe((response) => {
       this.data = response;
     });
+  }
+
+  nextPage() {
+    if (this.data.info.next != null) {
+      var nextPageUrl = this.data.info.next;
+      return this.postsService.getItems(nextPageUrl).subscribe((response) => {
+        this.data = response;
+      });
+    } else {
+      return 0;
+    }
+  }
+
+  prevPage() {
+    if (this.data.info.prev != null) {
+      var prevPageUrl = this.data.info.prev;
+      return this.postsService.getItems(prevPageUrl).subscribe((response) => {
+        this.data = response;
+      });
+    } else {
+      return 0;
+    }
   }
 }
